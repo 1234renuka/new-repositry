14811bc270e4dd22a88423e2bb808f6ac963eed7
Renuka Thakur <thakurrenuka8858@gmail.com>
2024-11-04 14:20:40 +0530

    new change

--- arry.c
+++ arry.c
@@ -1,4 +1,4 @@
-/*#include<stdio.h>
+#include<stdio.h>
 int main(){
     int arr[5] = {1,2,3,4,5};
     for(int i=0; i<=5; i++)
@@ -7,29 +7,10 @@ int main(){
         printf("\n");
     }
     return 0;
-}*/
-#include<stdio.h>
-int main(){
- int arr[500];
-    int n=0;
-    printf("how many numbers you  want to add in array");
-    scanf("%d",&n);
+}
 
-    for(int i=0; i<n; i++){
-        printf("%d",i);
-         
-       
-        
-       }
-       printf("enter the numbers ");
-       scanf("%d",&n);
-       for(int i=0; i<n; i++){
-        printf("%d", 2*i);
-        printf("\n");
-    }
     
     
     
    
-    return 0;
-}
+  
